89657641d709f9879e1a06b2d7a0c6611af1f9e1
alex-pythonista <indrajit.alex@gmail.com>
2024-03-22 05:39:39 +0600

    implemented list view

--- src/product/models.py
+++ src/product/models.py
@@ -8,16 +8,34 @@ class Variant(TimeStampMixin):
     description = models.TextField()
     active = models.BooleanField(default=True)
 
+    class Meta:
+        ordering = ('id',)
+
+    def __str__(self) -> str:
+        return self.title
+
 
 class Product(TimeStampMixin):
     title = models.CharField(max_length=255)
     sku = models.SlugField(max_length=255, unique=True)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
+
+    class Meta:
+        ordering = ('id',)
+
+    def __str__(self) -> str:
+        return self.title
 
 
 class ProductImage(TimeStampMixin):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     file_path = models.URLField()
+
+    class Meta:
+        ordering = ('id',)
+
+    def __str__(self) -> str:
+        return self.product.title
 
 
 class ProductVariant(TimeStampMixin):
@@ -25,14 +43,28 @@ class ProductVariant(TimeStampMixin):
     variant = models.ForeignKey(Variant, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
+    class Meta:
+        ordering = ('id',)
+
+    def __str__(self) -> str:
+        return self.variant_title
+
 
 class ProductVariantPrice(TimeStampMixin):
-    product_variant_one = models.ForeignKey(ProductVariant, on_delete=models.CASCADE, null=True,
+    product_variant_one = models.ForeignKey(ProductVariant, on_delete=models.CASCADE, blank=True, null=True,
                                             related_name='product_variant_one')
-    product_variant_two = models.ForeignKey(ProductVariant, on_delete=models.CASCADE, null=True,
+    product_variant_two = models.ForeignKey(ProductVariant, on_delete=models.CASCADE, blank=True, null=True,
                                             related_name='product_variant_two')
-    product_variant_three = models.ForeignKey(ProductVariant, on_delete=models.CASCADE, null=True,
+    product_variant_three = models.ForeignKey(ProductVariant, on_delete=models.CASCADE, blank=True, null=True,
                                               related_name='product_variant_three')
     price = models.FloatField()
     stock = models.FloatField()
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('product',)
+
+    def __str__(self) -> str:
+        variants = filter(None, [self.product_variant_one, self.product_variant_two, self.product_variant_three])
+        return ', '.join(str(variant) for variant in variants)
+        # return f"{self.product}::{self.price}"
--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,6 +1,9 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.views import generic
 
-from product.models import Variant
+from product.models import (
+    Variant, ProductVariantPrice)
 
 
 class CreateProductView(generic.TemplateView):
@@ -11,4 +14,20 @@ class CreateProductView(generic.TemplateView):
         variants = Variant.objects.filter(active=True).values('id', 'title')
         context['product'] = True
         context['variants'] = list(variants.all())
+        return context
+
+
+class ProductVariantPriceListView(generic.ListView):
+    model = ProductVariantPrice
+    template_name = 'products/list2.html'
+    context_object_name = 'product_variant_prices'
+    paginate_by = 2
+    
+    def get_queryset(self):
+        return ProductVariantPrice.objects.all().order_by('id')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        product_variant_prices = ProductVariantPrice.objects.select_related('product').all().order_by('product_id', 'created_at')
+        context['grouped_product_variant_prices'] = product_variant_prices
         return context
